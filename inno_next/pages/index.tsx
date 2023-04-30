e9ff63d51595232c149dba7a541d1d586e6639ab
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-04-30 14:39:52 +0530

    <UI> : [adding dashboardUI and problem, innovation routing]

--- inno_next/pages/index.tsx
+++ inno_next/pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Landing page comes here!</h1>
-    </>
+    <div>
+        <div className="container mx-auto md:px-8 px-4">
+            <h1 className='text-3xl font-bold underline'>Landing page comes here!</h1>
+        </div>
+    </div>
   )
 }
---
+++ inno_next/pages/innovation/[slug].tsx
@@ -0,0 +1,7 @@
+export default function Innovation() {
+    return (
+        <div className="container mx-auto">
+            <h1>Innovation page comes here!</h1>
+        </div>
+    )
+}
