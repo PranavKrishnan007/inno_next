3fcd1f01ca8124fe55e596c073d870d16f5ae1f0
Aashray Katiyar <aashray446@gmail.com>
2023-05-04 19:01:10 +0530

    <Changed> :[Core entity]

--- inno_next/next.config.js
+++ inno_next/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     API_TOKEN:
       'f371715c6cb0caa8b3b1db450a93284da1469835b80c50612df52e655cb81783887ee9251848b5826f0c3433318316ce8f35db0c1b3a9f592c98df959ac0515001534976aa96107997a59b73a231d8c01d8308cd6f268e95ad44d46d2eeaf631b77e7df4a2db744a8dafd662992a444ce506fbddebfb9f13b745ff5f0d6694e3',
-    SERVER_URL: 'https://localhost:1337'
+    SERVER_URL: 'http://localhost:1337'
   },
   images: {
     remotePatterns: [
--- inno_next/utils/Interfaces/coreEntity.ts
+++ inno_next/utils/Interfaces/coreEntity.ts
@@ -1,49 +1,50 @@
 export interface IStrapiServerData {
-    id: number
-    attributes: IInnovation | IProblem | IHackathon;
+  id: number
+  attributes: IInnovation | IProblem | IHackathon
 }
 
 export interface IInnovation {
-    id? : number
-    innocationId?: string
-    title: string
-    description: string
-    header_img: string
-    tags?: number[] | string[] 
-    badges?: string[]
-    status: string
-    createdBy?: string
+  id?: number
+  innocationId?: string
+  title: string
+  description: string
+  header_img: string
+  tags?: number[] | string[]
+  badges?: string[]
+  status: string
+  createdBy?: string
+  content: string
 }
 
 export interface IProblem {
-    id? : number
-    title: string
-    description: string
-    header_img: string
-    tags: number[] | string[]
-    badges: string[]
-    status: string
-    createdBy?: string
-    innovations?: IInnovation[]
+  id?: number
+  title: string
+  description: string
+  header_img: string
+  tags: number[] | string[]
+  badges: string[]
+  status: string
+  createdBy?: string
+  innovations?: IInnovation[]
+  content: string
 }
 
 export interface IHackathon {
-    id? : number
-    title: string
-    tagline : string
-    description: string
-    header_img: string
-    partcipantLimit: number
-    applicationOpenDate: Date
-    applicationCloseDate: Date
-    hackathonStartDate: Date
-    hackathonEndDate: Date
-    contactDetails: string
-    additionalDetails: string
-    tags:number[] | string[]
-    badges: string[] | number[]
-    participants: string[],
-    accepted?: boolean,
-    enduser? : number
+  id?: number
+  title: string
+  tagline: string
+  description: string
+  header_img: string
+  partcipantLimit: number
+  applicationOpenDate: Date
+  applicationCloseDate: Date
+  hackathonStartDate: Date
+  hackathonEndDate: Date
+  contactDetails: string
+  additionalDetails: string
+  tags: number[] | string[]
+  badges: string[] | number[]
+  participants: string[]
+  accepted?: boolean
+  enduser?: number
 }
-
