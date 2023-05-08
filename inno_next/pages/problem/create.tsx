cc7187b9aab36f15c509d8ec1d4e6b2543181b91
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-05-08 22:25:41 +0530

    <upload> : [image preview]

--- inno_next/pages/problem/create.tsx
+++ inno_next/pages/problem/create.tsx
@@ -37,6 +37,21 @@ export default function CreateProblem() {
                     </div>
                 </div>
                 <form className='bg-white rounded-b-xl flex flex-col p-4 md:p-8'>
+                    <div className="flex flex-row items-center justify-center bg-grey-lighter gap-4">
+                        <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-blue-800">
+                            <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20">
+                                <path
+                                  d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
+                                />
+                            </svg>
+                            <span className="mt-2 text-base leading-normal">Select a file</span>
+                            <input type='file' className="hidden"/>
+                        </label>
+                        <div>
+                            <img src={problem.header_img || '/assets/c20-tst-cover.jpg'} alt="" className="flex object-cover" width="700px" height="300px"/>
+                        </div>
+                    </div>
                     <div className='pb-4'>
                         <label className='block text-gray-700 text-2xl font-medium mb-2'>
                             Problem Title
