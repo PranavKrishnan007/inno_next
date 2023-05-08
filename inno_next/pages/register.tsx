d0ea7716a8229abe6df0425c153e3565275c8936
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-05-09 01:29:13 +0530

    <UI> : [updated the UI of register with password function missing]

--- inno_next/pages/register.tsx
+++ inno_next/pages/register.tsx
@@ -1,10 +1,8 @@
 import RegisterForm from "@/components/registrationForm/register"
 
 export default function Register() {
-  
+
   return (
-    <div className='flex flex-row h-screen bg-sky-400/20 overflow-hidden'>
-      <RegisterForm/>
-    </div>
+    <RegisterForm/>
   )
 }
