f6c5130f95b188ba63cf0062d475d21508b56c66
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-05-09 12:20:40 +0530

    <UI> : <remove unwanted div>

--- inno_next/pages/home.tsx
+++ inno_next/pages/home.tsx
@@ -146,109 +146,106 @@ export default function Listings() {
     const {push} = useRouter();
 
     return (
-        <div className="">
-            <div className="min-h-screen container mx-auto px-4 md:px-8 md:py-10 relative">
-                <div className="bg-white sticky flex flex-col top-0 z-50">
-                    <Branding />
-                    <div className="flex flex-row gap-10 pb-2 h-fit min-h-[80vh]">
-                        <div className="flex flex-col gap-4 w-3/4 pt-4">
-                            <div className="flex flex-row w-full gap-2 ">
-                                <div className="bg-white w-full p-2 rounded-full">
-                                    <div className="flex flex-row h-full justify-center shadow gap-1 p-1 items-center rounded-md bg-gray-300/30">
-                                        {tabs.map((tabName, index) => (
-                                            <div className={clsx([
-                                                "flex justify-center items-center w-1/3 h-full p-2 rounded-md  font-semibold hover:bg-primary/80  hover:text-white transition duration-300 ease-in-out",
-                                                activeTab === index ? "bg-primary text-white" : "text-gray-500"
-                                            ])}
-                                                 onClick={() => setActiveTab(index)}
-                                            >
-                                                {tabName}
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-row justify-between items-center gap-4">
-                                <div className="w-2/3 h-full rounded-full border-2 border-gray-300/30 p-1 bg-white">
-                                    <div className="rounded-full w-full shadow">
-                                        <InputWithButton />
-                                    </div>
-                                </div>
-                                <div className="w-1/3 bg-gray-300/30 p-1 rounded-lg border-2 border-white">
-                                    <Button
-                                        className="bg-primary hover:bg-orange-500/90 w-full text-white"
-                                        onClick={() => {activeTab === 0 ? push('/problem/create') : activeTab === 1 ? push('/innovation/create') : push('/home')}}
-                                    >
-                                        {activeTab === 0 ? "Create Problems" : activeTab === 1 ? " Create Innovations" : "Create Hackathons"}
-
-                                    </Button>
-                                </div>
-                            </div>
-                            <div className="flex justify-between">
-                                <p className="text-xl px-2 font-bold text-gray-700 tracking-wide w-full border-t pt-3">{activeTab === 0 ? "Problems" : activeTab === 1 ? "Innovations" : "Hackathons"}</p>
-                            </div>
-                            <div className="flex flex-row gap-10">
-                                <div className="flex flex-col gap-4 w-full">
-                                    <div className="bg-white rounded-2xl p-2">
-                                        <div className=" border-2   flex flex-col  rounded-2xl p-2">
-                                            {activeTab === 0 &&
-                                                testProblem.map((problem, index) => (
-                                                    <ProblemCard {...problem} key={index} header_img="" tags={['something']} content={'this is the content.'}/>
-                                                ))
-                                            }
-                                            {activeTab === 1 &&
-                                                innovations.map((innovation, index) => (
-                                                    <InnovationCard {...innovation} key={index}/>
-                                                ))}
-                                            {activeTab === 2 &&
-                                                hackathons.map((hackathon, index) => (
-                                                    <HackathonCard {...hackathon} key={index}/>
-                                                ))}
+        <div className="min-h-screen container mx-auto px-4 md:px-8 md:py-10 relative">
+            <div className="bg-white sticky flex flex-col top-0 z-50">
+                <Branding />
+                <div className="flex flex-row gap-10 pb-2 h-fit min-h-[80vh]">
+                    <div className="flex flex-col gap-4 w-3/4 pt-4">
+                        <div className="flex flex-row w-full gap-2 ">
+                            <div className="bg-white w-full p-2 rounded-full">
+                                <div className="flex flex-row h-full justify-center shadow gap-1 p-1 items-center rounded-md bg-gray-300/30">
+                                    {tabs.map((tabName, index) => (
+                                        <div className={clsx([
+                                            "flex justify-center items-center w-1/3 h-full p-2 rounded-md  font-semibold hover:bg-primary/80  hover:text-white transition duration-300 ease-in-out",
+                                            activeTab === index ? "bg-primary text-white" : "text-gray-500"
+                                        ])}
+                                             onClick={() => setActiveTab(index)}
+                                        >
+                                            {tabName}
                                         </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-row justify-between items-center gap-4">
+                            <div className="w-2/3 h-full rounded-full border-2 border-gray-300/30 p-1 bg-white">
+                                <div className="rounded-full w-full shadow">
+                                    <InputWithButton />
+                                </div>
+                            </div>
+                            <div className="w-1/3 bg-gray-300/30 p-1 rounded-lg border-2 border-white">
+                                <Button
+                                    className="bg-primary hover:bg-orange-500/90 w-full text-white"
+                                    onClick={() => {activeTab === 0 ? push('/problem/create') : activeTab === 1 ? push('/innovation/create') : push('/home')}}
+                                >
+                                    {activeTab === 0 ? "Create Problems" : activeTab === 1 ? " Create Innovations" : "Create Hackathons"}
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <p className="text-xl px-2 font-bold text-gray-700 tracking-wide w-full border-t pt-3">{activeTab === 0 ? "Problems" : activeTab === 1 ? "Innovations" : "Hackathons"}</p>
+                        </div>
+                        <div className="flex flex-row gap-10">
+                            <div className="flex flex-col gap-4 w-full">
+                                <div className="bg-white rounded-2xl p-2">
+                                    <div className=" border-2   flex flex-col  rounded-2xl p-2">
+                                        {activeTab === 0 &&
+                                            testProblem.map((problem, index) => (
+                                                <ProblemCard {...problem} key={index} header_img="" tags={['something']} content={'this is the content.'}/>
+                                            ))
+                                        }
+                                        {activeTab === 1 &&
+                                            innovations.map((innovation, index) => (
+                                                <InnovationCard {...innovation} key={index}/>
+                                            ))}
+                                        {activeTab === 2 &&
+                                            hackathons.map((hackathon, index) => (
+                                                <HackathonCard {...hackathon} key={index}/>
+                                            ))}
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="pt-4 w-1/4 ">
-                            <div className="border bg-white my-3 p-1 mx-1  rounded-full w-ful">
-                                <MultiSelect
-                                    data={['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8']}
-                                    placeholder="Select a tag"
-                                    styles={(theme) => ({
-                                        input: {
-                                            '&:focus-within': {
-                                                borderColor: theme.colors.orange[7],
-                                            },
+                    </div>
+                    <div className="pt-4 w-1/4 ">
+                        <div className="border bg-white my-3 p-1 mx-1  rounded-full w-ful">
+                            <MultiSelect
+                                data={['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8']}
+                                placeholder="Select a tag"
+                                styles={(theme) => ({
+                                    input: {
+                                        '&:focus-within': {
+                                            borderColor: theme.colors.orange[7],
                                         },
-                                    })}
-                                    transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }}
-                                    searchable
-                                    nothingFound='No tags found'
-                                    radius="xl"
-                                    variant="filled"
-                                />
-                            </div>
-                            <div className="flex flex-col bg-white rounded-full h-fit mt-5 p-2">
-                                <div className="h-full bg-gray-300/30 shadow  rounded-full">
-                                    <div className="flex justify-center p-2 items-center h-full font-semibold text-gray-700">
-                                        Notices
-                                    </div>
+                                    },
+                                })}
+                                transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }}
+                                searchable
+                                nothingFound='No tags found'
+                                radius="xl"
+                                variant="filled"
+                            />
+                        </div>
+                        <div className="flex flex-col bg-white rounded-full h-fit mt-5 p-2">
+                            <div className="h-full bg-gray-300/30 shadow  rounded-full">
+                                <div className="flex justify-center p-2 items-center h-full font-semibold text-gray-700">
+                                    Notices
                                 </div>
                             </div>
-                            <div className="pt-4 flex flex-col w-full h-fit sticky ">
-                                <div className="bg-white rounded-2xl p-2">
-                                    <div className=" flex flex-col  rounded-2xl p-3">
-                                        {NOTICE.map((notice) => (
-                                            <div className="flex flex-col gap-1 hover:shadow-xl hover:border-r-4 border-b-2 px-3 py-2 bg-white  transition duration-350 ease-in-out ">
-                                                <div className="font-bold text-sm text-gray-700 border-b border-white">
-                                                    {notice.title}
-                                                </div>
-                                                <div className="text-gray-500 font-light text-sm">
-                                                    {notice.description}
-                                                </div>
+                        </div>
+                        <div className="pt-4 flex flex-col w-full h-fit sticky ">
+                            <div className="bg-white rounded-2xl p-2">
+                                <div className=" flex flex-col  rounded-2xl p-3">
+                                    {NOTICE.map((notice) => (
+                                        <div className="flex flex-col gap-1 hover:shadow-xl hover:border-r-4 border-b-2 px-3 py-2 bg-white  transition duration-350 ease-in-out ">
+                                            <div className="font-bold text-sm text-gray-700 border-b border-white">
+                                                {notice.title}
                                             </div>
-                                        ))}
-                                    </div>
+                                            <div className="text-gray-500 font-light text-sm">
+                                                {notice.description}
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
--- inno_next/pages/innovation/create.tsx
+++ inno_next/pages/innovation/create.tsx
@@ -8,57 +8,55 @@ export default function CreateInnovation() {
     console.log(quill, quillRef)
 
     return (
-        <div className="bg-background">
-            <div className="min-h-screen container mx-auto px-4 md:px-8 md:py-10 relative">
-                <div className="bg-white sticky flex flex-col top-0 z-50">
-                    <Branding />
-                    <div className='bg-background pt-4'>
-                        <div className='bg-white text-secondary font-bold p-4 md:p-8 md:text-5xl text-2xl rounded-t-xl border-gray-300/30 border-b-2'>
-                            Submit Innovation
+        <div className="min-h-screen container mx-auto px-4 md:px-8 md:py-10 relative">
+            <div className="bg-white sticky flex flex-col top-0 z-50">
+                <Branding />
+                <div className='mt-4'>
+                    <div className='bg-white text-secondary font-bold p-4 md:p-8 md:text-5xl text-2xl rounded-t-xl border-gray-300/30 border-b-2'>
+                        Submit Innovation
+                    </div>
+                </div>
+            </div>
+            <form className='bg-white rounded-b-xl flex shadow-2xl flex-col p-4 md:p-8'>
+                <div className='pb-4'>
+                    <label className='block text-gray-700 text-2xl font-medium mb-2'>
+                        Innovation Title
+                    </label>
+                    <input
+                        onChange={() => null}
+                        className='appearance-none border border-gray-500/40 rounded-xl w-full p-4 text-gray-700 leading-tight placeholder:text-lg'
+                        id='innovationTitle'
+                        type='text'
+                        placeholder='Innovation Title'
+                    />
+                </div>
+                <div className='pb-4'>
+                    <label className='block text-gray-700 text-2xl font-medium mb-2'>
+                        Description
+                    </label>
+                    <div className='w-full h-72'>
+                        <div style={{ height: "100%", width: "100%" }}>
+                            <div ref={quillRef} className='rounded-b-xl' />
                         </div>
                     </div>
                 </div>
-                <form className='bg-white rounded-b-xl flex flex-col p-4 md:p-8'>
-                    <div className='pb-4'>
-                        <label className='block text-gray-700 text-2xl font-medium mb-2'>
-                            Innovation Title
-                        </label>
-                        <input
-                            onChange={() => (null)}
-                            className='appearance-none border border-gray-500/40 rounded-xl w-full p-4 text-gray-700 leading-tight placeholder:text-lg'
-                            id='innovationTitle'
-                            type='text'
-                            placeholder='Innovation Title'
-                        />
-                    </div>
-                    <div className='pb-4'>
-                        <label className='block text-gray-700 text-2xl font-medium mb-2'>
-                            Description
-                        </label>
-                        <div className='w-full h-72'>
-                            <div style={{ height: "100%", width: "100%" }}>
-                                <div ref={quillRef} className='rounded-b-xl' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full bg-white md:pt-10 pt-28 rounded-lg">
-                        <label className='block text-gray-700 text-2xl font-medium mb-2'>
-                            Tags
-                        </label>
-                        <MultiSelect
-                            data={['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8']}
-                            placeholder="Select a tag"
-                            transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }}
-                            searchable
-                            className='focus:border-primary focus:border-2'
-                            nothingFound='No tags found'
-                        />
-                    </div>
-                    <div className='pt-5'>
-                        <Button type='submit' variant='outline'>Submit for Moderation</Button>
-                    </div>
-                </form>
-            </div>
+                <div className="w-full bg-white md:pt-10 pt-28 rounded-lg">
+                    <label className='block text-gray-700 text-2xl font-medium mb-2'>
+                        Tags
+                    </label>
+                    <MultiSelect
+                        data={['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8']}
+                        placeholder="Select a tag"
+                        transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }}
+                        searchable
+                        className='focus:border-primary focus:border-2'
+                        nothingFound='No tags found'
+                    />
+                </div>
+                <div className='pt-5'>
+                    <Button type='submit' variant='outline'>Submit for Moderation</Button>
+                </div>
+            </form>
         </div>
     )
 }
