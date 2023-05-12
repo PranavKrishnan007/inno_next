c0ae4d6ca0f049ed9a28e7b46b40e06877bb5a43
Aashray Katiyar <aashray446@gmail.com>
2023-05-12 16:53:12 +0530

    <TEMP> : [Credentials for backend]

--- inno_next/next.config.js
+++ inno_next/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_TOKEN:
-      '9faf40dac427e3a5a944b1517c6761772d92a7a946674534f1ae2aa837568569206ad4651fc083337c68a5ec2689e818885614cbfc0d91270fe6a42f6d89a70c3d691cb1f4ff75abe76045026adb55016fb86291c95987a2b01b1eb51dd0631abdfd6658f1cb30c5f89be048821ea00724acdb2e5f51855c99b680cbaa9be7fc',
-    SERVER_URL: 'http://localhost:1337'
+      '5d4e00209e92ca1a73d4acd1d4a83e5faf0c9f41fe0cce30ca713b86ca4c2064ffefcb0bdf2aafbf9c95a46b04ca2ac96173726d088e9373165974338409782ea176774ecc29dc6b5ca66dbd77e60d583be10b86eef58338b8a17bc434082f65c4b716d0dbc7f7a91a3fb17e9a67ea92be1d1525c0a13342371c980e4d8aeaa3',
+    SERVER_URL: 'http://65.0.125.87:1337'
   },
   images: {
     remotePatterns: [
--- inno_next/utils/Services/axios_setup.ts
+++ inno_next/utils/Services/axios_setup.ts
@@ -1,40 +1,38 @@
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import Cookies from 'js-cookie'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import Cookies from 'js-cookie';
 
-const token = Cookies.get('token') ? Cookies.get('token') : process.env.API_TOKEN
+const token = Cookies.get('token') ? Cookies.get('token') : process.env.API_TOKEN;
 const headers = {
-  'Content-Type': 'application/json',
-  Authorization: `Bearer ${token}`,
-}
+	'Content-Type': 'application/json',
+	'Authorization': `Bearer ${token}`,
+};
 
 const axhttp = axios.create({
-  baseURL: 'http://localhost:1337/api',
-  headers,
-})
+	baseURL: process.env.SERVER_URL + '/api',
+	headers,
+});
 
 axhttp.interceptors.request.use(async (config) => {
-  return config
-})
+	return config;
+});
 
 axhttp.interceptors.response.use(
-  async (response) => {
-    return response.data
-  },
-  async (error) => {
-    console.log(error)
-    if (error.response?.data?.error) {
-      if (error.response.data.error.details?.details) {
-        toast.error(
-          error.response.data.error.details?.details.errors[0].message +
-            ' ' +
-            error.response.data.error.details?.details.errors[0].path[0],
-        )
-        return
-      }
-    }
-    toast.error(error.response?.data?.error?.message)
-  },
-)
+	async (response) => {
+		return response.data;
+	},
+	async (error) => {
+		console.log(error);
+		if (error.response?.data?.error) {
+			if (error.response.data.error.details?.details) {
+				toast.error(
+					error.response.data.error.details?.details.errors[0].message + ' ' + error.response.data.error.details?.details.errors[0].path[0],
+				);
+				return;
+			}
+		}
+		toast.error(error.response?.data?.error?.message);
+	},
+);
 
-export { axhttp }
+export { axhttp };
